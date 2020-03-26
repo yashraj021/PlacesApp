677921c0aa1e61117b61fcb347e81c2d88892486
yashraj021 <yash.raj021@gmail.com>
2020-03-26 23:09:03 +0530

    Added UsersList and UserItem component. Added USers page.

--- src/users/pages/Users.jsx
+++ src/users/pages/Users.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
 
 const Users = () => {
 
-    return <h1>Users Works!</h1>
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Yash',
+            image: 'https://avatars2.githubusercontent.com/u/47192627?s=460&u=887f008eec63433d4904794be842ce515776bf03&v=4',
+            places: 3
+        },
+    ]
+
+    return <UsersList items={USERS} />
 }
 
 export default Users;
